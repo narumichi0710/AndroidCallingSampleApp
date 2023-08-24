0796d918282b806c6c398ec1bc77cae182235964
Narumichi Kubo <narumichi7373@gmail.com>
2023-08-24 22:37:41 +0900

    着信音とバイブレーションを設定

--- app/src/main/java/com/example/androidcallingsampleapp/service/TelecomConnection.kt
+++ app/src/main/java/com/example/androidcallingsampleapp/service/TelecomConnection.kt
@@ -3,6 +3,10 @@ package com.example.androidcallingsampleapp.service
 
 import android.content.Context
 import android.content.Intent
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.telecom.Connection
 import android.telecom.DisconnectCause
 import android.util.Log
@@ -28,6 +32,13 @@ class TelecomConnection(
     private val context: Context,
     private val stateChangedListeners: MutableList<ConnectionStateChangedListener>
 ) : Connection() {
+    // 着信音
+    private val ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+    private val ringtone: Ringtone = RingtoneManager.getRingtone(context, ringtoneUri)
+
+    // バイブレーション
+    private val vibrator: Vibrator? = context.getSystemService(Vibrator::class.java)
+    private val pattern = longArrayOf(0, 400, 200, 400)
 
     init {
         connectionProperties = PROPERTY_SELF_MANAGED
@@ -44,10 +55,14 @@ class TelecomConnection(
     override fun onAnswer() {
         Log.d(tag,"onAnswer")
         setActive()
+        vibrator?.cancel()
+        ringtone.stop()
     }
 
     override fun onReject() {
         Log.d(tag,"onReject")
+        vibrator?.cancel()
+        ringtone.stop()
         setDisconnected(DisconnectCause(DisconnectCause.REJECTED))
     }
 
@@ -58,6 +73,8 @@ class TelecomConnection(
 
     override fun onShowIncomingCallUi() {
         Log.d(tag,"onShowIncomingCallUi, $extras")
+        vibrator?.vibrate(VibrationEffect.createWaveform(pattern, 0))
+        ringtone.play()
         val intent = Intent(context, IncomingCallActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
             action = Intent.ACTION_VIEW
